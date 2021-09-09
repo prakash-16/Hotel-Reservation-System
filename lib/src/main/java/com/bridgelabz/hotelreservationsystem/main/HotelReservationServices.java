26b04f367c4313ca1ca5d8913f6d233d5fc67b52
Prakash Goud <prakash.goud@morningstar.com>
2021-09-09 23:42:18 +0530

    [Prakash] - Added the feature to give rating of cheapest hotel in given data range

--- lib/src/main/java/com/bridgelabz/hotelreservationsystem/main/HotelReservationServices.java
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/main/HotelReservationServices.java
@@ -12,7 +12,7 @@ import java.util.Map.Entry;
 
 public class HotelReservationServices {
 	public static int i = 0;
-	public static HashMap<String,Integer> bill = new HashMap<>();
+	public static HashMap<String, Integer> bill = new HashMap<>();
 
 	public static void cheapestHotel(ArrayList<HotelReservationSystem> hotelList, CustomerType customerType,
 			String checkInDate, String checkOutDate) {
@@ -21,47 +21,52 @@ public class HotelReservationServices {
 		LocalDate checkOutDay = LocalDate.parse(checkOutDate, format);
 		int noOfDays = (checkOutDay.getDayOfMonth() - checkInDay.getDayOfMonth()) + 1;
 		hotelList.stream().forEach(n -> {
-			if(checkInDay.getDayOfWeek().equals(DayOfWeek.SATURDAY) && checkOutDay.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
-				if(customerType.equals(CustomerType.REWARD)) {
-					bill.put(n.getHotelName(), noOfDays*(n.getRate().get(CustomerType.REWARD).getWeekendRates()));
+			if (checkInDay.getDayOfWeek().equals(DayOfWeek.SATURDAY)
+					&& checkOutDay.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+				if (customerType.equals(CustomerType.REWARD)) {
+					bill.put(n.getHotelName(), noOfDays * (n.getRate().get(CustomerType.REWARD).getWeekendRates()));
+				} else {
+					bill.put(n.getHotelName(), noOfDays * (n.getRate().get(CustomerType.REGULAR).getWeekendRates()));
 				}
-				else {
-					bill.put(n.getHotelName(), noOfDays*(n.getRate().get(CustomerType.REGULAR).getWeekendRates()));
+			} else if (checkInDay.getDayOfWeek().equals(DayOfWeek.SUNDAY)
+					&& checkOutDay.getDayOfWeek().equals(DayOfWeek.MONDAY)) {
+				if (customerType.equals(CustomerType.REWARD)) {
+					bill.put(n.getHotelName(), ((n.getRate().get(CustomerType.REWARD).getWeekdayRates())
+							+ (n.getRate().get(CustomerType.REWARD).getWeekendRates())));
+				} else {
+					bill.put(n.getHotelName(), ((n.getRate().get(CustomerType.REGULAR).getWeekdayRates())
+							+ (n.getRate().get(CustomerType.REGULAR).getWeekendRates())));
 				}
-			}
-			else if(checkInDay.getDayOfWeek().equals(DayOfWeek.SUNDAY) && checkOutDay.getDayOfWeek().equals(DayOfWeek.MONDAY)) {
-				if(customerType.equals(CustomerType.REWARD)) {
-					bill.put(n.getHotelName(), ((n.getRate().get(CustomerType.REWARD).getWeekdayRates()) + (n.getRate().get(CustomerType.REWARD).getWeekendRates())));
+			} else if (checkInDay.getDayOfWeek().equals(DayOfWeek.FRIDAY)
+					&& checkOutDay.getDayOfWeek().equals(DayOfWeek.SATURDAY)) {
+				if (customerType.equals(CustomerType.REWARD)) {
+					bill.put(n.getHotelName(), ((n.getRate().get(CustomerType.REWARD).getWeekdayRates())
+							+ (n.getRate().get(CustomerType.REWARD).getWeekendRates())));
+				} else {
+					bill.put(n.getHotelName(), ((n.getRate().get(CustomerType.REGULAR).getWeekdayRates())
+							+ (n.getRate().get(CustomerType.REGULAR).getWeekendRates())));
 				}
-				else {
-					bill.put(n.getHotelName(), ((n.getRate().get(CustomerType.REGULAR).getWeekdayRates()) + (n.getRate().get(CustomerType.REGULAR).getWeekendRates())));
-				}
-			}
-			else if(checkInDay.getDayOfWeek().equals(DayOfWeek.FRIDAY) && checkOutDay.getDayOfWeek().equals(DayOfWeek.SATURDAY)) {
-				if(customerType.equals(CustomerType.REWARD)) {
-					bill.put(n.getHotelName(), ((n.getRate().get(CustomerType.REWARD).getWeekdayRates()) + (n.getRate().get(CustomerType.REWARD).getWeekendRates())));
-				}
-				else {
-					bill.put(n.getHotelName(), ((n.getRate().get(CustomerType.REGULAR).getWeekdayRates()) + (n.getRate().get(CustomerType.REGULAR).getWeekendRates())));
-				}
-			}
-			else {
-				if(customerType.equals(CustomerType.REWARD)) {
-					bill.put(n.getHotelName(), noOfDays*(n.getRate().get(CustomerType.REWARD).getWeekdayRates()));
-				}
-				else {
-					bill.put(n.getHotelName(), noOfDays*(n.getRate().get(CustomerType.REGULAR).getWeekdayRates()));
+			} else {
+				if (customerType.equals(CustomerType.REWARD)) {
+					bill.put(n.getHotelName(), noOfDays * (n.getRate().get(CustomerType.REWARD).getWeekdayRates()));
+				} else {
+					bill.put(n.getHotelName(), noOfDays * (n.getRate().get(CustomerType.REGULAR).getWeekdayRates()));
 				}
 			}
 		});
 		int minPrice = Collections.min(bill.values());
-		for(Entry<String, Integer> mapElement : bill.entrySet()) {
-			String key = (String)mapElement.getKey();
-			int value = ((int)mapElement.getValue());
-			if(value == minPrice) {
-				System.out.println("The cheapest hotel is " + key + " and the price is " + minPrice + "$.");
+		for (Entry<String, Integer> mapElement : bill.entrySet()) {
+			String key = (String) mapElement.getKey();
+			int value = ((int) mapElement.getValue());
+			if (value == minPrice) {
+				hotelList.stream().forEach(n -> {
+					if (n.getHotelName().equals(key)) {
+						System.out.println("The cheapest hotel is " + key + " with " + n.getHotelRating()
+								+ " star rating " + "and the price is " + minPrice + "$.");
+					}
+				});
 			}
 		}
-  }
-	
+	}
+
 }
