fef571863f950a737b143dd7da67e8d6dce509f8
Prakash Goud <prakash.goud@morningstar.com>
2021-09-10 19:27:37 +0530

    [Prakash] - Added the exception class to check invalid entries

---
+++ lib/src/test/java/com/bridgelabz/hotelreservationsystem/test/FormatException.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hotelreservationsystem.test;
+
+public class FormatException extends Exception {
+	
+	public FormatException(String message) {
+		System.out.println(message);
+	}
+
+}
